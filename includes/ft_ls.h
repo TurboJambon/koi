24f36e21cdd7e07ae97607977e987fed21e3f657
TurboJambon <david.chirol@wanadoo.fr>
2017-05-12 18:13:51 +0200

    le sort marche pas

--- includes/ft_ls.h
+++ includes/ft_ls.h
@@ -6,7 +6,7 @@
 /*   By: dchirol <dchirol@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/12 14:58:24 by dchirol           #+#    #+#             */
-/*   Updated: 2017/05/12 15:14:59 by dchirol          ###   ########.fr       */
+/*   Updated: 2017/05/12 17:05:42 by dchirol          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,51 +16,65 @@
 # include "libft.h"
 
 # include <dirent.h>
-# include <pwd.h>
-# include <uuid/uuid.h>
 # include <sys/stat.h>
-# include <time.h>
-# include <stdio.h>
+# include <pwd.h>
 # include <grp.h>
-# include <uuid/uuid.h>
-# include <unistd.h>
-# include <stdlib.h>
+# include <time.h>
 # include <errno.h>
 # include <sys/types.h>
 # include <sys/acl.h>
 # include <sys/xattr.h>
 
-# define OPTIONS "Rafglprtu"
+# define RED   			"\x1B[31m"
+# define GRN   			"\x1B[32m"
+# define YEL   			"\x1B[33m"
+# define BLU   			"\x1B[34m"
+# define MAG   			"\x1B[35m"
+# define CYN   			"\x1B[1;96m"
+# define WHT   			"\x1B[37m"
+# define RESET 			"\x1B[0m"
 
-//COLORS
-# define RED   "\x1B[31m"
-# define GRN   "\x1B[32m"
-# define YEL   "\x1B[33m"
-# define BLU   "\x1B[34m"
-# define MAG   "\x1B[35m"
-# define CYN   "\x1B[1;96m"
-# define WHT   "\x1B[37m"
-# define RESET "\x1B[0m"
+# define LSL			0x01
+# define LSA			0x02
+# define LSR			0x04
+# define LST			0x08
+# define LSU			0x10
+# define LSF			0x20
+# define LSG			0x40
+# define LSD			0x80
+# define LSRM			0x100
+# define LSP			0x200
 
-//TYPEDEF
-typedef unsigned long int		t_ulint;
-typedef unsigned char			t_uchar;
+# define OPTL			(flags & LSL)
+# define OPTA			(flags & LSA)
+# define OPTR			(flags & LSR)
+# define OPTT			(flags & LST)
+# define OPTU			(flags & LSU)
+# define OPTF			(flags & LSF)
+# define OPTG			(flags & LSG)
+# define OPTD			(flags & LSD)
+# define OPTRM			(flags & LSRM)
 
-//BUF FUNCTIONS
-int								ft_buf(int fd, void *str, int size);
+# define LS_NAME		dirent->d_name
+# define LS_TYPE		dirent->d_type
+# define LS_MODE		stat->mode
+# define LS_NLINK		stat->nlink
+# define LS_UNAME		stat->uname
+# define LS_GRNAME		stat->grname
+# define LS_SIZE		stat->size
 
-void							ft_putchar_buf(char c);
-void							ft_putchar_buf_fd(char c, int fd);
-void							ft_putcharendl_buf_fd(char c, int fd);
+typedef struct s_stats	t_my_stats;
+typedef struct stat		t_stat;
+typedef struct dirent	t_dirent;
+typedef unsigned int	t_uint;
 
-void							ft_putstr_buf(char *str);
-void							ft_putstr_buf_fd(char *str, int fd);
-void							ft_putendl_buf_fd(char *str, int fd);
-
-void							ft_putnbr_buf(int n);
-
-//STATS FUNCTIONS
-void							print_mode(mode_t n);
-int								typestat(struct stat stats);
+struct s_my_stats
+{
+	t_stat				*stat;
+	t_dirent			*dirent;
+	char				*path;
+	char				*gid;
+	char				*uid;
+};
 
 #endif
--- src/ft_ls.c
+++ src/ft_ls.c
@@ -5,10 +5,170 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dchirol <dchirol@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/05/12 14:44:07 by dchirol           #+#    #+#             */
-/*   Updated: 2017/05/12 14:58:11 by dchirol          ###   ########.fr       */
+/*   Created: 2017/05/12 16:34:58 by dchirol           #+#    #+#             */
+/*   Updated: 2017/05/12 18:13:21 by dchirol          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "ft_ls.h"
 
+void			ft_affarg(char **av, int ac)
+{	
+	int i;
+
+	i = 0;
+	while (i < ac)
+	{
+		ft_putendl(av[i]);
+		i++;
+	}
+}
+
+int				ft_ls(char *s, t_uint prout)
+{
+	(void)s;
+	(void)prout;
+	return (1);
+}
+
+int				is_folder(char *name)
+{
+	return (name[0] == 'Z');
+}
+
+void			ft_swaptab(char **s1, char **s2)
+{
+	char *tmp;
+
+	tmp = *s1;
+	*s1 = *s2;
+	*s2 = tmp;
+}
+
+void			sort_str(char **av, int size)
+{
+	int i;
+	int flag;
+
+	flag = 1;
+	while (flag)
+	{
+		i = 0;
+		flag = 0;
+		while (i < size)
+		{
+			if (ft_strcmp(av[i], av[i + 1]) > 0)
+			{
+				ft_swaptab(av + i, av + i + 1);
+				flag = 1;
+			}
+			i++;
+		}
+	}
+
+}
+
+void			sort_params(char **av, int ac)
+{
+	int i;
+	int j;
+
+	i = 0;
+	j = ac - 1;
+
+	while (i < ac && !is_folder(av[i]))
+		i++;
+	while (j >= 0 && is_folder(av[j]))
+		j--;
+	while (i < j)
+	{
+		if (is_folder(av[i]))
+		{
+			ft_swaptab(av + i, av + j);
+			i++;
+			j--;
+		}
+		else
+			i++;
+	}
+	ft_affarg(av, ac);
+	sort_str(av, j - 1);
+	sort_str(av + j - 1, ac - j - 1);
+	ft_putendl("---------");
+	ft_affarg(av, ac);
+}
+
+void			ft_multi_ls(char **av, int ac, t_uint flags)
+{
+	int i;
+
+	i = 0;
+	while (i < ac)
+	{
+		ft_ls(av[i], flags);
+		i++;
+	}
+}
+
+void			fill_tab(t_uint **tab)
+{
+	*tab = (t_uint*)malloc(sizeof(**tab) * 127);
+	ft_bzero(*tab, 127 * 4);
+	(*tab)['l'] = LSL;
+	(*tab)['a'] = LSA;
+	(*tab)['r'] = LSR;
+	(*tab)['t'] = LST;
+	(*tab)['u'] = LSU;
+	(*tab)['f'] = LSF;
+	(*tab)['d'] = LSD;
+	(*tab)['R'] = LSRM;
+	(*tab)['G'] = LSG;
+	(*tab)['p'] = LSP;
+}
+
+void		 	get_flags(char *av, t_uint *flags)
+{
+	static t_uint 	*tab = NULL;
+
+	if (!tab)
+		fill_tab(&tab);
+	while (*av)
+	{
+		if (tab[(int)*av])
+			*flags |= tab[(int)*av];
+		else
+		{
+			ft_putstr("illegal options"); //A RAJOUTER DANS LIBFT
+			exit(0);
+		}
+		av++;
+	}
+}
+
+
+
+int 			main(int ac, char **av)
+{
+	t_uint	flags;
+	int		i;
+
+	flags = 0;
+	if (ac < 2)
+		return (ft_ls(ft_strdup("."), flags));
+	i = 1;
+	while (i < ac && av[i][0] == '-' && av[i][1] != '\0')
+	{
+		get_flags(av[i] + 1, &flags);
+		i++;
+	}
+	av += i;
+	ac -= i;
+	if (!ac)
+		return(ft_ls(ft_strdup("."), flags));
+	if (ac == 1)
+		return(ft_ls(ft_strdup(*av), flags));
+	sort_params(av, ac);
+	ft_multi_ls(av, ac, flags);
+	return (0);
+}
